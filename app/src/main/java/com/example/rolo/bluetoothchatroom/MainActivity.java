2307801729f92a386b38130797999b519fc3f747
Rolo <rolo_lamperouge@foxmail.com>
2017-03-12 23:47:45 +0800

    add setDiscoverable

--- app/src/main/java/com/example/rolo/bluetoothchatroom/MainActivity.java
+++ app/src/main/java/com/example/rolo/bluetoothchatroom/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.rolo.bluetoothchatroom;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    //local bluetooth adapter
+    private BluetoothAdapter myBluetoothAdapter;
+    //intent request codes
+    private static final int REQUEST_ENABLE_BT = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(!testBlootoAvailable())
+        if(!testBluetoothAvailable())
             finish();
-
+        activateBluetooth();
+        setDiscoverable();
     }
-
-    private boolean testBlootoAvailable(){
-        BluetoothAdapter myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    //ask for default bluetooth adapter, if failed return false which result in the termination of app
+    private boolean testBluetoothAvailable(){
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(myBluetoothAdapter == null){
             Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
             return false;
@@ -26,7 +31,23 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    private void avtivateBluetooth(){
+    //set the current device to discoverable
+    private void activateBluetooth(){
+        if(!myBluetoothAdapter.isEnabled()){
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+    }
+    //set current device to discoverable
+    private void setDiscoverable(){
+        if(myBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE){
+            Intent setDiscoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            setDiscoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(setDiscoverableIntent);
+        }
+    }
+    //start device discover with the BluetoothAdapter
+    private void scanBluetoothDevice(){
 
     }
 }
